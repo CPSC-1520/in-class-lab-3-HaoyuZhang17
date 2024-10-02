fbd7988a7e227c9b5ebe7c60dc0c30baa33803b2
HaoyuZhang17 <156087337+HaoyuZhang17@users.noreply.github.com>
2024-10-01 21:49:09 -0600

    finish lab3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 // Enter your code below.
+let div = document.querySelector('.javascript-resources');
+let button = document.querySelector('#show-resources');
+
+button.addEventListener("click", evenHandler);
+function evenHandler() {
+    div.classList.remove('d-none');
+}
+
+
+div.addEventListener("mouseover", (event)=> {
+    
+    console.log(event.target)
+    event.target.classList.add("fw-bold")
+    
+})
+div.addEventListener("mouseout", (event)=> {
+    console.log(event.target)
+  
+    event.target.classList.remove("fw-bold")
+})
+div.addEventListener("click", (event)=> {
+    console.log(event.target)
+  
+    event.target.classList.add("fst-italic")
+})
